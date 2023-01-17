b554970376d02e49ec6150e780f4e7c3ddbba1ae
Jun0613-spec <kumo5110@gmail.com>
2023-01-17 11:22:51 +0000

    weather forecast app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="flex ml-auto w-full font-bold">
+      <p className="text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400 cursor-pointer">
+        Weather
+      </p>
+    </div>
+  );
+}
+
+export default Header;
